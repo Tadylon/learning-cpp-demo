7b3b7030cb858385e21f28a2ea7c3571503f7b32
tadylon <enthuswine@gmail.com>
2026-07-16 01:24:17 +0800

    it`s so hard to learn input stream and the buffer and the so many get , getline , cin.get , cin.getline

--- STL/stream/get_serise.cpp
+++ STL/stream/get_serise.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 #include "print_obj.h"
 
 
 int main() {
-        char szBuf[6];
-        char szBuf1[6];
+        char szBuf[6] = "faff";
+        char szBuf1[6] = "asfd";
         std::cout << "szBuf : ";
         cout__(szBuf);
         std::cout << "szBuf1 : ";
@@ -13,9 +14,21 @@ int main() {
         // cline__("\n");
 
         std::cout << "please input number : ";
-        int32_t n = std::cin.get();        //特殊情况可使用get提取'\n'
+        // int32_t n = std::cin.get();        //特殊情况可使用get提取'\n'
+        std::string n_string;
+        
+        std::cout << "pls input string : ";
+        std::getline(std::cin,n_string);
+        std::cout << "n_string : ";
+        cout__(n_string);
+
+        std::cout << "pls input number : ";
+        int n = std::cin.get();
         std::cout << "n : ";
         cout__(n);
+        char ch = std::cin.get(); // 假设输入 1
+        std::cout << "ch : "<<  ch << "\n";          // 输出：1
+
 
         //2. 将std::get(szBuf1 , 6) 换成 std::getline()
         std::cin.getline(szBuf1, 6);
@@ -24,7 +37,8 @@ int main() {
         //专门使用一个来解决掉全部buffer
         
         cline__("----------------------\n");
-        std::cin.getline(szBuf, 6);
+
+        std::cin.getline(szBuf,sizeof(szBuf));
         // std::cout << n << std::endl;
         std::cout << "The received string is:" << szBuf << std::endl;
         //cout << "The received string is:" << szBuf1 << endl;
